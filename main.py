f150c70a8b2d7b994bc3b104eab4336ee05d8e5b
Johan <pradojohan805@gmail.com>
2026-05-15 20:54:01 -0500

    Agregue el Endpoint de crear tarea

--- main.py
+++ main.py
@@ -1,43 +1,49 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-
+from typing import Optional
 app = FastAPI()
 db_tareas = {}
-task_id = 0
+task_id = 1
 
+class TareaCreate(BaseModel):
+    titulo: str
+    descripcion: Optional[str] = None
 
 class Tarea(BaseModel):
+    id: int
     titulo: str
-    descripcion: str | None
+    descripcion: Optional[str] = None
     completada: bool = False
 
 @app.get("/tasks")
 def get_tasks():
     """retorna todas las tareas"""
-    return db_tareas 
+    return db_tareas
 
 @app.post("/tasks")
-def crear_task(task: Tarea):
+def crear_tarea(task: TareaCreate):
+    """crea tarea nueva"""
     global task_id
+
+    # Verificar si ya existe una tarea con el mismo título
+    for tarea in db_tareas.values():
+        if tarea["titulo"].lower() == task.titulo.lower():
+            raise HTTPException(
+                status_code=400,
+                detail="La tarea ya existe"
+            )
+
+    nueva_tarea = Tarea(
+        id=task_id,
+        titulo=task.titulo,
+        descripcion=task.descripcion
+    )
+
+    db_tareas[task_id] = nueva_tarea.model_dump()
+
     task_id += 1
-    """crea una tarea nueva"""
-    
-    nuevo_id = task_id
 
-    db_tareas[nuevo_id] = task.model_dump()
+    return {
+        "mensaje": "Tarea creada con éxito"
+    }
 
-    return {"mensaje": "Tarea creada con exito"}
-
-
-
-
-
-
-"""    if usuario.user_id in usuarios_db:
-        raise HTTPException(status_code=400, detail="El ID de usuario ya existe")
-    
-    # Agregar al diccionario de diccionarios
-    usuarios_db[usuario.user_id] = {
-        "nombre": usuario.nombre,
-        "rol": usuario.rol
-    }"""

81bb5ad44384717afbac4766b69e10cad913e227
Johan <pradojohan805@gmail.com>
2026-05-15 19:44:08 -0500

    Endpoint de ver tareas

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
+app = FastAPI()
+db_tareas = {}
+task_id = 0
+
+
+class Tarea(BaseModel):
+    titulo: str
+    descripcion: str | None
+    completada: bool = False
+
+@app.get("/tasks")
+def get_tasks():
+    """retorna todas las tareas"""
+    return db_tareas
